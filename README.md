3d3b276285d305bf7f3fdd51266069fd232f901e
Wave FC <malloriepinto89@gmail.com>
2023-05-03 18:29:43 -0700

    updated README and working on hourChange function

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Work-Day-Scheduler
+
+## Description
+
+This is a simple work day scheduler app.  This was created using day.js, javascrip, css and JQuery.  The goal was to dispaly the current day, date and time at the top.  I was then to use javascript and jQuery to make the app more dynamtic by color blocking the hours based on the acutal current time.  I also used localstorage to save the events that are typed in by the user even if the page had been refreshed.  
+
+## Installation
+
+Work Day Scheduler URL:
+
+![Alt text](images/Screenshot.png)
+
+## Usage
+The time blocks are broken up by the hour between 8am -5pm.  The user is able to type in events or todos for each hour that will save on the page once the save icon is clicked.  The hour blocks will also be color coded dependent on the current time.  Green for the future, red for the current hour and grey for the past.  
+ 
+
+## License
+
+No License
--- script.js
+++ script.js
@@ -66,16 +66,16 @@ $(".saveBtn").on("click", function () {
     localStorage.setItem(time, text);
 })
 //keep already saved tasks on the scheduler.//
-$("#hour-8 .description").val(localStorage.getItem("hour-8"));
-$("#hour-9.description").val(localStorage.getItem("hour-9"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
 //identify the current time
 //split the number from the ID
@@ -84,16 +84,19 @@ $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 //remove the class and add class dependent on the hour
  
 function hourChange () {
-    var currentHour = dayjs().hour('HH'); 
-    var blockHour = id $(this.attr('id'));
+    var currentHour = dayjs().hour(); 
+    var blockHour = parseInt($(this).attr('id'));
     
-    for (var i = 0; i < blockHour; i += 1) 
+    for (var i = 0; i < blockHour; i += 1) //question
 
     if (currentHour === id) {
         $(timeBlockEl.addClass)('present');
     }  else if (currentHour > id) {
         $(timeBlockEl.addClass)('past');
-    }  else (currentHour < id) {
+    }  else {
         $(timeBlockEl.addClass)('future');
-    } 
+    }
+        
 }
+
+hourChange();
